7df054600c6a1580bbac480f561b95e10c7ad795
Tea Unnebäck <teaunneback@Teas-MacBook-Air-2.local>
2021-10-28 12:37:37 +0200

    version 1, only brute force function included

---
+++ R/brute_force_knapsack.R
@@ -0,0 +1,36 @@
+#' Brute force solve for the knapsack problem
+#'
+#' A function that uses brute force to solve the knapsack problem
+#'
+#' @param x A data frame with two variables w = weight and v = value
+#'
+#' @param W A numeric scalar with the maximum weight the knapsack is allowed to contain
+#'
+#' @return The function returns a list with the optimum packed value and the corresponding elements
+#'
+#' @examples
+#' n <- 10
+#' knapsack_objects <- data.frame(w = sample(1:4000, size = n, replace = TRUE),
+#'                             v = runif(n = n, 0, 10000))
+#' brute_force_knapsack(x = knapsack_objects, W = 3500)
+#'
+#' @import dplyr
+#'
+#' @export
+brute_force_knapsack <- function(x, W){
+  if(!is.data.frame(x)) stop("x is not a data frame!")
+  if(ncol(x) != 2) stop("x does not have correct dimension!")
+  if(colnames(x)[1] != "w" & colnames(x)[2] != "v") stop("Columns in x are not named correctly!")
+  if(any(x < 0)) stop("Faulty values in x!")
+  possible_combinations <- data.frame("weight" = NA, "value" = NA)
+  for (i in 1:(2^(nrow(x)))){
+    possible_combinations[i, "weight"] <-
+      sum(knapsack_objects[which(as.numeric(intToBits(i)[1:nrow(x)]) == 1), 1])
+    possible_combinations[i, "value"] <-
+      sum(knapsack_objects[which(as.numeric(intToBits(i)[1:nrow(x)]) == 1), 2])
+  }
+  output <- possible_combinations %>% filter(weight <= W) %>% slice_max(value, n = 1)
+  elements <- which(as.numeric(intToBits(which(
+    possible_combinations[ , "value"] == output[1, "value"]))[1:nrow(x)]) == 1)
+  return(list(value = output[ , "value"], elements = elements))
+}
---
+++ R/help.R
@@ -0,0 +1,11 @@
+#' the knapsack package
+#'
+#' The package contains different functions for solving what is called the knapsack problem. The knapsack problem is a discrete optimization problem where we have a knapsack that can take a limited weight W and we want to fill this knapsack with a number of items i = 1, ..., n, each with a weight w[i] and a value v[i]. The goal is to find the knapsack with the largest value of the elements added to the knapsack.
+#'
+#'
+#' @docType package
+#'
+#' @author Tea Unnebäck and Karl Lindberg \email{tea@folkdatorn.se}
+#'
+#' @name knapsack
+NULL
